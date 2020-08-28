d373efa6bb7a7d1a190ff24eafab9d7afefce6c8
endormi <39559256+endormi@users.noreply.github.com>
2020-08-28 17:14:55 +0300

    Minor additions to macros

--- macros/macros.lua
+++ macros/macros.lua
@@ -9,11 +9,16 @@ and the UID is '378940FB'.
 
 Note: This code will be updated in the future (for now it's purpose is to be an boilerplate, but it will be changed).
 
+This code should work for multiple keyboards.
+
 --]]
 
+-- lmc_minimize();
+-- lmc_reset();
 -- you can also use: lmc_assign_keyboard('device')
 lmc_print_devices()
 lmc_device_set_name('kbd', 'pick_your_uid') -- It should be behind 00#&& ....... and ends in the last &
+-- lmc.minimizeToTray = false
 
 lmc_set_handler(
   'kbd', function(button, direction)
@@ -52,7 +57,44 @@ lmc_set_handler(
   end)
 
 
+-- Second keyboard
+lmc_device_set_name('kbd2', 'pick_your_uid')
 
+lmc_set_handler(
+  'kbd2', function(button, direction)
+  if (direction == 1) then return end
+    if (button == 59) then
+      lmc_send_keys('^+{I}')
+
+    elseif (button == string.byte('N')) then
+      lmc_spawn('code.cmd')
+
+    elseif (button == 45) then
+      lmc_send_keys('Text')
+
+    elseif (button == 68) then
+      lmc_send_keys('^a^v')
+
+    elseif (button == 83) then
+      lmc_send_keys('^a^c')
+
+    elseif (button == 87) then
+      lmc_send_keys('^z')
+
+    elseif (button == 88) then
+      lmc_send_keys('^x')
+
+    elseif (button == 91) then
+      lmc_send_keys('^s')
+
+    elseif (button == 92) then
+      lmc_spawn('notepad', 'c:\\file.txt')
+
+    else
+      print('Not an assigned button: ' .. button)
+
+    end
+  end)
 
 
 
